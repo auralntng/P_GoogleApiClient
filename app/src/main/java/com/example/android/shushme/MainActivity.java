b59b129ec965693304054d7af01adbc8baefffe7
auralntng <auralintang77@gmail.com>
2017-09-22 21:45:32 +0700

    selesai

---
+++ app/src/main/java/com/example/android/shushme/MainActivity.java
@@ -0,0 +1,124 @@
+package com.example.android.shushme;
+
+/*
+* Copyright (C) 2017 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*  	http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiCLient;
+import com.google.android.gms.common.api.GoogleApiCLient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Places;
+
+public class MainActivity extends AppCompatActivity implements
+        ConnectionCallback,
+        OnConnectionFailedListener {
+
+    // Constants
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private static final int PERMISSIONS_REQUEST_FINE_LOCATION = 111;
+
+    // Member variables
+    private PlaceListAdapter mAdapter;
+    private RecyclerView mRecyclerView;
+
+    {
+        locationPermissions.setChecked(true);
+        locationPermissions.setEnabled(false);
+    }
+
+    /**
+     * Called when the activity is starting
+     *
+     * @param savedInstanceState The Bundle that contains the data supplied in onSaveInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Set up the recycler view
+        mRecyclerView = (RecyclerView) findViewById(R.id.places_list_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mAdapter = new PlaceListAdapter(this);
+        mRecyclerView.setAdapter(mAdapter);
+
+        // TODO (4) Create a GoogleApiClient with the LocationServices API and GEO_DATA_API
+
+        GoogleApiCLient client = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnCOnnectionFailedListener(this)
+                .AddApi(LocationServices.API)
+                .addApi(Places.GEO_DATA_API)
+                .enableAutoManage(this, this)
+                .build();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle connectionHint) {
+        Log.i(TAG, "API Client Conection Successful!");
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        Log.i(TAG, "API Client Connection Suspended!");
+    }
+
+    // TODO (5) Override onConnected, onConnectionSuspended and onConnectionFailed for GoogleApiClient
+    // TODO (7) Override onResume and inside it initialize the location permissions checkbox
+    // TODO (8) Implement onLocationPermissionClicked to handle the CheckBox click event
+    // TODO (9) Implement the Add Place Button click event to show  a toast message with the permission status
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult result) {
+        Log.e(TAG, "API Client Connection Failed!");
+    }
+
+    public void OnAddPlaceButtonClicked(View view) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            Troast, makeText(this, getString(R.string.need_location_permission_message), Toast.LENGTH_LONG).show();
+            return;
+        }
+        Toast.makeText(this, getString(R.string.location_permission_granted_message), Toast.LENGTH_LONG).show();
+    } else
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        CheckBox locationPermissions = (CheckBox) findViewById(R.id.location_permission_checkbox);
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            locationPermissions.setChecked(false);
+    }
+}
+
+    public void onLocationPermissionsClicked(View view) {
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_FINE_LOCATION);
+    }
